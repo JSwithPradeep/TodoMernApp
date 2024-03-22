6a97d4a2cc99e8352603651d6f1a526f55d135b6
Pradeep Singh <130471506+JSwithPradeep@users.noreply.github.com>
2024-03-22 00:26:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express"
 import mongoose  from "mongoose";
 import router from "./router/todoRouter.js";
 import dotenv from "dotenv"
-import { fileURLToPath } from 'url';
-import path from "path";
+import {fileURLToPath} from "url";
+import path from "path"
 
 dotenv.config();
 
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 18000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static((path.join(__dirname, './client/build'))))
 
 mongoose
   .connect(process.env.DATABSE, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,8 +29,7 @@ mongoose
 app.use("/api", router)
 
 // static filse
-app.use(express.static(path.join(__dirname,"./client/build")));
-app.get("*", function(req, res){
-  res.sendFile(path.join(__dirname, "./client/build/index.html"))
+app.use('*', function(req,res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
 app.listen(PORT, () => console.log(`Listning at ${PORT}...`));
